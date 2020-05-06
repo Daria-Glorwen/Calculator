a3083e0275988e3387b80069ff32ad99d5fa92f2
Daria D <darja.dolbe@gmail.com>
2020-05-06 17:50:21 +0300

    creating interface

---
+++ Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+            InitializeCalculator();
+        }
+
+        private void InitializeCalculator()
+        {
+            this.BackColor = Color.Bisque;
+
+            string buttonName = null;
+            Button button = null;
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                buttonName = "button" + i;
+                button = (Button)this.Controls[buttonName];
+                button.Text = i.ToString();
+            }
+
+            button1.Text = "1";
+            button2.Text = "2";
+            button3.Text = "3";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
